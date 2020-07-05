c8c024f0b2d4f82a39e5c19582946739b5b28926
etienne-napoleone <etienne.napoleone@gmail.com>
2020-07-05 19:31:27 +0900

    feat(cli): add test gauge

--- price_exporter/cli.py
+++ price_exporter/cli.py
@@ -1,2 +1,13 @@
+import time
+
+from prometheus_client import Gauge
+from prometheus_client import start_http_server
+
+gauge_rate_luna_krw = Gauge("rate_luna_krw", "Rate for luna/krw")
+
+
 def entrypoint() -> None:
-    pass
+    gauge_rate_luna_krw.set(10.2)
+    start_http_server(8000)
+    while True:
+        time.sleep(10)
