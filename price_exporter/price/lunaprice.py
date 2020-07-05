2cf830df89c2c3689c07251b1e28522ed85fce58
etienne-napoleone <etienne.napoleone@gmail.com>
2020-07-05 21:39:19 +0900

    refactor(price): use a base class

---
+++ price_exporter/price/lunaprice.py
@@ -0,0 +1,24 @@
+from typing import Union
+
+from prometheus_client.metrics import MetricWrapperBase
+import poche
+
+from price_exporter import helpers
+
+
+class LunaPrice:
+    def __init__(self, ttl: int, metric: MetricWrapperBase) -> None:
+        self._cache = poche.Cache(ttl)
+        self._metric = metric
+        self._metric.set_function(self.get)
+
+    def get(self) -> Union[float, str]:
+        price = helpers.NOT_A_NUMBER
+        try:
+            price = self._cache.get("price")
+        except KeyError:
+            pass
+        return price
+
+    def fetch(self) -> None:
+        raise NotImplementedError
--- price_exporter/price/ukrw.py
+++ price_exporter/price/ukrw.py
@@ -1,25 +1,15 @@
-from typing import Union
-
 from prometheus_client import Gauge
-import poche
 import requests
 
 from price_exporter import helpers
+from price_exporter.price.lunaprice import LunaPrice
 
 
-class Ukrw:
-    def __init__(self, ttl: int = 30) -> None:
-        self._cache = poche.Cache(ttl)
-        self._metric = Gauge("price_ukrw", "Price of luna in ukrw")
-        self._metric.set_function(self.get)
-
-    def get(self) -> Union[float, str]:
-        price = helpers.NOT_A_NUMBER
-        try:
-            price = self._cache.get("price")
-        except KeyError:
-            pass
-        return price
+class Ukrw(LunaPrice):
+    def __init__(self, ttl: int) -> None:
+        LunaPrice.__init__(
+            self, ttl=ttl, metric=Gauge("price_ukrw", "Price of luna in ukrw")
+        )
 
     def fetch(self) -> None:
         price = helpers.NOT_A_NUMBER
