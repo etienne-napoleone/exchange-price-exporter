65776e63b356a44feb98c1b4a3a5e3b2a4c9440b
etienne-napoleone <etienne.napoleone@gmail.com>
2020-07-06 20:08:32 +0900

    feat(config): refactor to toml

--- terra_price_exporter/config.py
+++ terra_price_exporter/config.py
@@ -1,13 +1,37 @@
-import os
+import toml
 
 
 class Config:
-    def __init__(self):
-        self.debug = os.getenv("DEBUG")
-        self.port = int(os.getenv("PORT", "8000"))
-        self.interval = int(os.getenv("INTERVAL", "10"))
-        self.denoms = (
-            [denom.strip() for denom in os.getenv("DENOMS").split(",")]
-            if os.getenv("DENOMS")
-            else []
+    def __init__(self, path: str) -> None:
+        with open(path) as f:
+            self._raw_config = toml.load(f)
+        self.server = ServerConfig(data=self._raw_config.get("server", {}))
+        self.log = LogConfig(data=self._raw_config.get("log", {}))
+        self.exporter = ExporterConfig(
+            data=self._raw_config.get("exporter", {})
         )
+
+
+class ServerConfig:
+    def __init__(self, data: dict) -> None:
+        self.port = int(data.get("port", 8000))
+
+
+class LogConfig:
+    def __init__(self, data: dict) -> None:
+        self.debug = bool(data.get("debug", False))
+
+
+class ExporterConfig:
+    def __init__(self, data: dict) -> None:
+        self.interval = int(data.get("interval", 10))
+        self.pairs = [
+            ExporterPairConfig(pair) for pair in list(data.get("pairs", {}))
+        ]
+
+
+class ExporterPairConfig:
+    def __init__(self, data: dict) -> None:
+        self.exchange = str(data["exchange"])
+        self.currency = str(data["currency"])
+        self.market = str(data["market"])
